c1b46643e81f56173ec573ba4d61a2447433a3d3
AndrewF17 <andrea.ferrario170700@gmail.com>
2021-08-25 16:07:24 +0200

    -

---
+++ Java Clsses to Import into the project/App.java
@@ -0,0 +1,23 @@
+import java.sql.SQLException;
+
+import util.DBManager;
+import classes.*;
+
+public class App {
+    public static void main(String[] args) {
+       
+        DBManager manager = new DBManager("tai.db.elephantsql.com", 
+        "igabhvra", "igabhvra", 
+        "pM8kEKh0soYm_ZRR_DcpIlelPAFb2UFv");
+        try {
+            manager.connect();
+        } catch (SQLException exception) {
+            System.err.println(exception.getMessage());
+        }
+
+        for (CentroVaccinale centroVaccinale : manager.getAllCentriVaccinali()) {
+            System.out.println(centroVaccinale.toString());
+        }
+
+    }
+}
