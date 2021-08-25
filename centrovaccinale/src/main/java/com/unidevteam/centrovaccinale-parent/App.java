ffe1a1478b1226b315fd2223cc5cac6de333bf03
AndrewF17 <andrea.ferrario170700@gmail.com>
2021-08-25 15:52:55 +0200

    -

--- centrovaccinale/src/main/java/com/unidevteam/centrovaccinale-parent/App.java
+++ centrovaccinale/src/main/java/com/unidevteam/centrovaccinale-parent/App.java
@@ -8,6 +8,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        DBManager manager = new DBManager("tai.db.elephantsql.com", 
+        "igabhvra", "igabhvra", 
+        "pM8kEKh0soYm_ZRR_DcpIlelPAFb2UFv");
+        try {
+            manager.connect();
+        } catch (SQLException exception) {
+            System.err.println(exception.getMessage());
+        }
+
+        for (CentroVaccinale centroVaccinale : manager.getAllCentriVaccinali()) {
+            System.out.println(centroVaccinale.toString());
+        }
+
     }
 }
