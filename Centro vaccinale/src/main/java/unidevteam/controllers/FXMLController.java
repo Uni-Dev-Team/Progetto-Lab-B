e116228c6f7e114044c22bb2a71d682cd55628a7
AndrewF17 <andrea.ferrario170700@gmail.com>
2021-08-27 09:45:02 +0200

    -

--- Centro vaccinale/src/main/java/unidevteam/controllers/FXMLController.java
+++ Centro vaccinale/src/main/java/unidevteam/controllers/FXMLController.java
@@ -14,8 +14,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import unidevteam.util.DBManager;
-
 
 public class FXMLController implements Initializable {
 
@@ -37,12 +35,5 @@ public class FXMLController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
        lb_test.setVisible(false);
-       DBManager mDbManager = new DBManager();
-       try {
-           mDbManager.connect();
-           System.out.println("Connection estabilished!");
-       } catch (Exception e) {
-            System.out.println("Connection not estabilished!");
-       }
     }
 }
