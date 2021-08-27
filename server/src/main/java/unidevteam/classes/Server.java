7a1aba9526668b1c971e38ae7c9b3a13e957ff28
Borotalcohol <loschiavo.christian@gmail.com>
2021-08-27 15:34:04 +0200

    Inizio implementazione RMI
    Co-authored-by: Andrea Ferrario <andrea@ferrario.us>
    Co-authored-by: Ivan Giubilei <Kawa-git@users.noreply.github.com>

---
+++ server/src/main/java/unidevteam/classes/Server.java
@@ -0,0 +1,55 @@
+package unidevteam.classes;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.sql.Date;
+import java.util.List;
+
+import unidevteam.enumerators.TipoVaccino;
+import unidevteam.interfaces.CentroVaccinaleInterfaccia;
+
+class Server extends UnicastRemoteObject implements CentroVaccinaleInterfaccia{
+
+    public Server() throws RemoteException {
+        super();
+
+        System.out.println("Registering ...");
+        Registry registry = LocateRegistry.createRegistry(1099);
+        registry.rebind("server", this);
+        System.out.println("Registered");
+    }
+
+    @Override
+    public CentroVaccinale getCentroVaccinaleById(String id) throws RemoteException {
+        System.out.println("hi");
+        return null;
+    }
+
+    @Override
+    public boolean registraCentroVaccinale(CentroVaccinale centro) throws RemoteException {
+        
+        return false;
+    }
+
+    @Override
+    public boolean cancellaCentroVaccinale(CentroVaccinale centro) throws RemoteException {
+        
+        return false;
+    }
+
+    @Override
+    public boolean registraVaccinato(String nomeCentro, String nomeCittadino, String cognomeCittadino,
+            String codiceFiscale, Date dataSomministrazione, TipoVaccino tipoVaccino, String idVaccinazione)
+            throws RemoteException {
+       
+        return false;
+    }
+
+    @Override
+    public List<CentroVaccinale> ottieniCentriVaccinali() throws RemoteException {
+        
+        return null;
+    }
+}
