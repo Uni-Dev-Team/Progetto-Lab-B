02abe0bd622096c001bb33cf67bc7e6bd664a578
Kawa-git <giubileiivan@gmail.com>
2021-08-27 15:46:09 +0200

    modifiche

--- Centro vaccinale/src/main/java/unidevteam/App.java
+++ Centro vaccinale/src/main/java/unidevteam/App.java
@@ -1,28 +1,29 @@
 package unidevteam;
 
-import java.io.File;
-import java.io.IOException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import unidevteam.interfaces.CentroVaccinaleInterfaccia;
 
 /**
  * Hello world!
  *
  */
-public class App extends Application
-{
-    public static void main( String[] args ){
+public class App extends Application{
+    public static void main( String[] args ) throws RemoteException, NotBoundException{
+        Registry registro = LocateRegistry.getRegistry(1099);
+        CentroVaccinaleInterfaccia server = (CentroVaccinaleInterfaccia) registro.lookup("server");
+        server.getCentroVaccinaleById("");
         launch(args);
-        
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(new File("src/main/resources/scene.fxml").toURI().toURL());
-        stage.setScene(new Scene(root));
-        stage.show();
+    public void start(Stage arg0) throws Exception {
+        
+        
     }
 }
---
+++ Centro vaccinale/src/main/java/unidevteam/interfaces/CentroVaccinaleInterfaccia.java
@@ -0,0 +1,15 @@
+package unidevteam.interfaces;
+import java.rmi.*;
+import java.sql.Date;
+import java.util.List;
+
+import unidevteam.classes.CentroVaccinale;
+import unidevteam.enumerators.TipoVaccino;
+
+public interface CentroVaccinaleInterfaccia extends Remote {
+    public boolean registraCentroVaccinale(CentroVaccinale centro) throws RemoteException;
+    public boolean cancellaCentroVaccinale(CentroVaccinale centro) throws RemoteException;
+    public boolean registraVaccinato(String nomeCentro, String nomeCittadino, String cognomeCittadino, String codiceFiscale, Date dataSomministrazione, TipoVaccino tipoVaccino, String idVaccinazione) throws RemoteException;
+    public List<CentroVaccinale> ottieniCentriVaccinali() throws RemoteException;
+    public CentroVaccinale getCentroVaccinaleById(String id) throws RemoteException;
+}
